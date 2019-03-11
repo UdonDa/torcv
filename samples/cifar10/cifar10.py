49efd917c0aac46c5e853c9f0b1f7c3ad6e9163f
UdonDa <udoooon0727@gmail.com>
2019-03-12 00:08:37 +0900

    fix gitignore

---
+++ samples/cifar10/cifar10.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as T
+from torch.backends import cudnn
+
+import argparse
+
+
+
+
+
+
+if __name__ == '__main__':
+        parser = argparse.ArgumentParser()
+
+    # Model configuration
+    parser.add_argument('--c_dim', type=int, default=6, help='dimension of domain labels (1st dataset)')
+    parser.add_argument('--c2_dim', type=int, default=6, help='dimension of domain labels (2nd dataset)')
+    # parser.add_argument('--celeba_crop_size', type=int, default=178, help='crop size for the CelebA dataset')
+    parser.add_argument('--rafd_crop_size', type=int, default=256, help='crop size for the RaFD dataset')
+    parser.add_argument('--image_size', type=int, default=256, help='image resolution')
+    parser.add_argument('--g_conv_dim', type=int, default=64, help='number of conv filters in the first layer of G')
+    parser.add_argument('--d_conv_dim', type=int, default=64, help='number of conv filters in the first layer of D')
+    parser.add_argument('--g_repeat_num', type=int, default=6, help='number of residual blocks in G')
+    parser.add_argument('--d_repeat_num', type=int, default=6, help='number of strided conv layers in D')
+    parser.add_argument('--lambda_cls', type=float, default=1, help='weight for domain classification loss')
+    parser.add_argument('--lambda_rec', type=float, default=10, help='weight for reconstruction loss')
+    parser.add_argument('--lambda_gp', type=float, default=10, help='weight for gradient penalty')
+    
+    # Training configuration.
+    parser.add_argument('--dataset', type=str, default='RaFD', choices=['CelebA', 'RaFD', 'Both'])
+    parser.add_argument('--batch_size', type=int, default=8, help='mini-batch size')
+    parser.add_argument('--num_iters', type=int, default=200000, help='number of total iterations for training D')
+    parser.add_argument('--num_iters_decay', type=int, default=100000, help='number of iterations for decaying lr')
+    parser.add_argument('--g_lr', type=float, default=0.0001, help='learning rate for G')
+    parser.add_argument('--d_lr', type=float, default=0.0001, help='learning rate for D')
+    parser.add_argument('--n_critic', type=int, default=5, help='number of D updates per each G update')
+    parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for Adam optimizer')
+    parser.add_argument('--beta2', type=float, default=0.999, help='beta2 for Adam optimizer')
+    parser.add_argument('--resume_iters', type=int, default=None, help='resume training from this step')
+    parser.add_argument('--selected_attrs', '--list', nargs='+', help='selected attributes for the CelebA dataset',
+                        default=['Black_Hair', 'Blond_Hair', 'Brown_Hair', 'Male', 'Young'])
+
+    # Test configuration.
+    parser.add_argument('--test_iters', type=int, default=200000, help='test model from this step')
+
+    # Miscellaneous.
+    parser.add_argument('--num_workers', type=int, default=8)
+    parser.add_argument('--mode', type=str, default='train', choices=['train', 'test'])
+    parser.add_argument('--use_tensorboard', type=str2bool, default=True)
+
+    parser.add_argument('--gpu_number', type=int, default=2, help='test model from this step')
+
+    # Additional Experiments
+    parser.add_argument('--use_attention', type=str2bool, default=True)
+    parser.add_argument('--use_spectral_norm_G', type=str2bool, default=True)
+    parser.add_argument('--use_spectral_norm_D', type=str2bool, default=True)
+
+    parser.add_argument('--name', type=str, default='name')
+    parser.add_argument('--root_dir', type=str, default='name')
+    parser.add_argument('--rafd_image_dir', type=str, default='data/RaFD/train')
+    parser.add_argument('--log_dir', type=str, default='{}/logs'.format(name))
+    parser.add_argument('--model_save_dir', type=str, default='{}/models'.format(name))
+    parser.add_argument('--sample_dir', type=str, default='{}/samples'.format(name))
+    parser.add_argument('--result_dir', type=str, default='{}/results'.format(name))
+
+    # Step size.
+    parser.add_argument('--log_step', type=int, default=10)
+    parser.add_argument('--sample_step', type=int, default=1000)
+    parser.add_argument('--model_save_step', type=int, default=10000)
+    parser.add_argument('--lr_update_step', type=int, default=1000)
+
+    config = parser.parse_args()
+    print(config)
+
+    main(config)
--- torcv/samples/cifar10.py
+++
@@ -1,6 +0,0 @@
-import torch
-import torch.nn as nn
-import torchvision
-import torchvision.transforms as T
-
-
