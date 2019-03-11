02ac37589138d1838f3c6e432130da1028148a07
UdonDa <udoooon0727@gmail.com>
2019-03-11 23:58:55 +0900

    fix gitignore

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## TORCV
+
+## Setup
+1. please clone this repository.
+2. `conda env create -f=torcv.yml`
---
+++ torcv/samples/cifar10.py
@@ -0,0 +1,6 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as T
+
+
