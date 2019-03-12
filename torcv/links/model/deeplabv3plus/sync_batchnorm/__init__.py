b6ece69db59b4270e72110baacc810c80a78e0bc
UdonDa <udoooon0727@gmail.com>
2019-03-12 14:32:56 +0900

    add xception

--- torcv/links/model/deeplabv3plus/sync_batchnorm/__init__.py
+++ torcv/links/model/deeplabv3plus/sync_batchnorm/__init__.py
@@ -1,1 +1,4 @@
-# Ref: https://github.com/vacancy/Synchronized-BatchNorm-PyTorch
+# Ref: https://github.com/vacancy/Synchronized-BatchNorm-PyTorchfrom
+
+from .batchnorm import SynchronizedBatchNorm1d, SynchronizedBatchNorm2d, SynchronizedBatchNorm3d
+from .replicate import DataParallelWithCallback, patch_replication_callback
